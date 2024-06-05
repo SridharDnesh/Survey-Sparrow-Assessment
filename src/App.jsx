28cabb6b96472b0f09d97928a8cd7245d4d5f39d
Sridhar <sridharan@crayond.co>
2024-06-05 19:24:00 +0530

    ✨ feat: MVP code complete for BarChart Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import BarChart from "./components/BarChart";
+import { generateRandomList } from "./utils/heplers";
+import { RATINGS, RATINGS_COUNT_TICKS } from "./utils/constants";
+import "./App.css";
+
+const ratings = [1, 2, 3, 4, 5];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [dataPoints, setDataPoints] = useState([10, 22, 12, 43, 9]);
+
+  const handleRegenerate = () => {
+    setDataPoints(generateRandomList(ratings.length));
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main>
+      <BarChart
+        data={{
+          xLabel: "Rating",
+          yLabel: "No. of ratings",
+          xRange: RATINGS,
+          yRange: RATINGS_COUNT_TICKS,
+          dataPoints: dataPoints,
+        }}
+        xRange={RATINGS}
+        yRange={RATINGS_COUNT_TICKS}
+        dataPoints={dataPoints}
+      />
+      <button onClick={handleRegenerate}>Regenerate</button>
+    </main>
+  );
 }
 
-export default App
+export default App;
