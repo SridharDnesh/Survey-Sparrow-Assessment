cadc38464d393c0329f1a40cdc920aee65ec35b8
Sridhar <sridharan@crayond.co>
2024-06-05 23:04:26 +0530

    ✨ feat: Error Boundary added

---
+++ src/components/ErrorBoundary/index.jsx
@@ -0,0 +1,28 @@
+import { Component } from "react";
+import PropTypes from "prop-types";
+
+class ErrorBoundary extends Component {
+  state = { hasError: false };
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, info) {
+    console.log(error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <div>Something went wrong.</div>;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
+
+ErrorBoundary.propTypes = {
+  children: PropTypes.element,
+};
